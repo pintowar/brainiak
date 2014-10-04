f5f2adfc314050ee6c37d30f9e90700b75daecca
Thiago Oliveira <pintowar@gmail.com>
2014-10-04 15:17:43 -0300

    bug fix on UI when triyng to solve a solved puzzle

--- brainiak-samples/src/main/scala/brainiak/samples/npuzzle/ui/controller/AStarController.scala
+++ brainiak-samples/src/main/scala/brainiak/samples/npuzzle/ui/controller/AStarController.scala
@@ -19,15 +19,17 @@ class AStarController(val b: Board) extends BasicController {
   override def startAction() = {
     new Thread(new Task[Unit]() {
       override def call(): Unit = {
-        board.controls.solvingStatus()
         val problem = NPuzzleProblem(board.puzzleState)
-        val search = GraphSearch()
-        val strategy = BestFirst(st => st - problem.goal)
-        val path = search.find(problem, strategy).path
-        board.controls.movingStatus()
-        path.foreach(n => n match {
-          case puzzle: NPuzzleNode => if (puzzle.movement != 0) move(puzzle.movement)
-        })
+        if (!problem.goalAchieved) {
+          board.controls.solvingStatus()
+          val search = GraphSearch()
+          val strategy = BestFirst(st => st - problem.goal)
+          val path = search.find(problem, strategy).path
+          board.controls.movingStatus()
+          path.foreach(n => n match {
+            case puzzle: NPuzzleNode => if (puzzle.movement != 0) move(puzzle.movement)
+          })
+        }
       }
     }).start()
 
