192d107c5d9cb29548ea8ee0c7bf68115d353f17
Thiago Oliveira Pinheiro <thiago_pinheiro@atlantico.com.br>
2014-10-09 09:28:00 -0300

    Refactoring Dynamic Programming impl

--- brainiak-core/src/test/scala/brainiak/optimum/DynamicProgrammingTest.scala
+++ brainiak-core/src/test/scala/brainiak/optimum/DynamicProgrammingTest.scala
@@ -3,6 +3,7 @@ package brainiak.optimum
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
+
 import scala.collection.BitSet
 
 /*import org.junit.runner.RunWith
@@ -14,25 +15,25 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class DynamicProgrammingTest extends FunSuite {
 
-  val dyn = new DynamicProgramming(List(8, 10, 15, 4), List(4, 5, 8, 3), 4, 11)
+
+  val dyn = new DynamicProgramming(List((8, 4), (10, 5), (15, 8), (4, 3)), 11)
+  val listBit = List(BitSet(4, 5, 6, 7, 8, 9, 10, 11), BitSet(5, 6, 7, 8, 9, 10, 11),
+    BitSet(8), BitSet(3, 7, 11))
 
   test("test mountTable") {
     val sol = dyn.mountTable
-    assert(sol == List(BitSet(4, 5, 6, 7, 8, 9, 10, 11), BitSet(5, 6, 7, 8, 9, 10, 11),
-                  BitSet(8), BitSet(3, 7, 11)))
+    assert(sol == listBit)
   }
 
   test("test backTracking") {
-    val sol = List(BitSet(4, 5, 6, 7, 8, 9, 10, 11), BitSet(5, 6, 7, 8, 9, 10, 11),
-      BitSet(8), BitSet(3, 7, 11))
-
-    assert(dyn.backTracking(sol) == List(0, 0, 1, 1))
+    assert(dyn.backTracking(listBit) == List(0, 0, 1, 1))
   }
 
   test("test solve") {
 
     val result = dyn.solve
     assert(result == List(0, 0, 1, 1))
+    assert(dyn.values == List(8, 10, 15, 4))
     assert((0 until dyn.items).map(it => dyn.values(it) * result(it)).sum == 19)
   }
 }
